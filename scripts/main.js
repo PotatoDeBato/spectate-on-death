df053c0b26c0938c86f75cef4f7959e62f6d0f0e
Potato <72053053+PotatoDeBato@users.noreply.github.com>
2026-05-13 19:08:42 -0700

    forgot to allow players to see the respawn screen

--- scripts/main.js
+++ scripts/main.js
@@ -1,8 +1,10 @@
 const respawn = document.getElementById("respawn");
+const respawnButton = document.getElementById("respawn-button");
 const respawnDefuse = document.getElementById("respawn-gm2");
 const observer = new MutationObserver(() => {
   if (getComputedStyle(respawn).display === 'block') {
     defly.spectate();
+    observer.disconnect();
   }
 });
 const observerDefuse = new MutationObserver(() => {
@@ -10,5 +12,10 @@ const observerDefuse = new MutationObserver(() => {
     defly.spectate();
   }
 });
+
+respawnButton.addEventListener("click", () => {
+  observer.observe(respawn, { attributes: true, attributeFilter: ['style'] });
+});
+
 observer.observe(respawn, { attributes: true, attributeFilter: ['style'] });
 observerDefuse.observe(respawnDefuse, { attributes: true, attributeFilter: ['style'] });
