4d6b7d6a6ad3c3ef6217bce35c043d41e460583d
Potato <72053053+PotatoDeBato@users.noreply.github.com>
2026-05-13 18:02:51 -0700

    Add observer for teams and defuse buttons

--- scripts/main.js
+++ scripts/main.js
@@ -1,13 +1,26 @@
 const respawn = document.getElementById("respawn");
+const respawnDefuse = document.getElementById("respawn-gm2");
 const respawnButton = document.getElementById("respawn-button");
+const respawnButtonDefuse = document.querySelector("#respawn-buttons-gm2 button");
 const observer = new MutationObserver(() => {
-  if (respawn.style.display === 'block') {
+  if (getComputedStyle(respawn).display === 'block') {
     defly.spectate();
     observer.disconnect();
+  }
+});
+const observerDefuse = new MutationObserver(() => {
+  if (getComputedStyle(respawnDefuse).display === 'block') {
+    defly.spectate();
+    observerDefuse.disconnect();
   }
 });
 respawnButton.addEventListener('click', () => {
   observer.observe(respawn, { attributes: true, attributeFilter: ['style'] });
 });
 
+respawnButtonDefuse.addEventListener('click', () => {
+  observerDefuse.observe(respawnDefuse, { attributes: true, attributeFilter: ['style'] });
+});
+
 observer.observe(respawn, { attributes: true, attributeFilter: ['style'] });
+observerDefuse.observe(respawnDefuse, { attributes: true, attributeFilter: ['style'] });
